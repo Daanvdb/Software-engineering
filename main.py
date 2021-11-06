9e5370694b5c113393f977fabd2fb34ccfc552b8
Bilal-B00 <bilal.00boughlala@gmail.com>
2021-11-06 15:03:23 +0100

    chatbot and translate module.

--- main.py
+++ main.py
@@ -7,4 +7,8 @@ michael = ChatBot('michael', only_read=True, database_uri='')
 dwight = ChatBot('dwight', only_read=True, database_uri='sqlite:///dwight.db')
 
 
+def nl_to_en(text_input):
+    return translator.translate(text_input, src='nl', dest='en').text
 
+def en_to_nl(text_input):
+    return translator.translate(text_input, src='en', dest='nl').text
