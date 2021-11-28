4977c32fb909bcdf4c0cdeafb2ba0f11393e8fbb
TychonBos <54850632+TychonBos@users.noreply.github.com>
2021-11-28 10:06:26 +0100

    GUI only (without chatbot and AI component)

--- Chatbot_GUI.py
+++ Chatbot_GUI.py
@@ -6,33 +6,16 @@ Created on Mon Nov  8 11:34:22 2021
 """
 
 from tkinter import *
-from googletrans import Translator
-from chatterbot import ChatBot
 #from chat import get_response, bot_name
-
-translator = Translator()
-
-michael = ChatBot('michael', only_read=True, database_uri='sqlite:///michael.db')
-dwight = ChatBot('dwight', only_read=True, database_uri='sqlite:///dwight.db')
-bot = dwight
 
 BG_GRAY = "#ABB2B9"
 BG_COLOR = "#17202A"
 TEXT_COLOR = "#EAECEE"
-translateT = False
 
 FONT = "Helvetica 14"
 FONT_BOLD = "Helvetica 13 bold"
 
-
-def nl_to_en(text_input):
-    return translator.translate(text_input, src='nl', dest='en').text
-
-def en_to_nl(text_input):
-    return translator.translate(text_input, src='en', dest='nl').text
-
 class ChatApplication:
-    
     
     def __init__(self):
         self.window = Tk()
@@ -40,31 +23,11 @@ class ChatApplication:
         
     def run(self):
         self.window.mainloop()
-
-    def _change_language(self, Event):                                       # _change_language and _change_personality have to be declared before declaring the buttons
-        global translateT                                                    # otherwise it might rise an error or get stuck in a loop
-        if translateT == True:
-            self.trans_button.config(text="NL")
-            translateT = False
-        else:
-            self.trans_button.config(text="EN")
-            translateT = True
-
-    def _change_personality(self,Event):
-        b = 2
-        global bot
-        if bot==dwight:
-            self.bot_pers.config(text="Dwight")
-            bot = michael
-        else:
-            self.bot_pers.config(text="Michael")
-            bot = dwight
-
         
     def _setup_main_window(self):
         self.window.title("Chat")
         self.window.resizable(width=False, height=False)
-        self.window.configure(width=950, height=550, bg=BG_COLOR)
+        self.window.configure(width=470, height=550, bg=BG_COLOR)
         
         # head label
         head_label = Label(self.window, bg=BG_COLOR, fg=TEXT_COLOR,
@@ -100,30 +63,14 @@ class ChatApplication:
         send_button = Button(bottom_label, text="Send", font=FONT_BOLD, width=20, bg=BG_GRAY,
                              command=lambda: self._on_enter_pressed(None))
         send_button.place(relx=0.77, rely=0.008, relheight=0.06, relwidth=0.22)
-
-    
-
-        # translate button
-        self.trans_button = Button(head_label, text="NL", font=FONT_BOLD, width=4, bg=BG_GRAY,
-                             command= lambda: self._change_language(None))
-        self.trans_button.place(x=890,y=0)
-
-        #Bot personality
-        self.bot_pers = Button(head_label, text="Michael", font=FONT_BOLD, width=6, bg=BG_GRAY,
-                             command= lambda: self._change_personality(None))
-        self.bot_pers.place(x=800,y=0)
      
     def _on_enter_pressed(self, event):
-        global bot
         msg = self.msg_entry.get()
-        self._insert_message(msg, "You", bot)
+        self._insert_message(msg, "You")
         
-    def _insert_message(self, msg, sender, bot):
-        global translateT
+    def _insert_message(self, msg, sender):
         if not msg:
             return
-
-    
         
         self.msg_entry.delete(0, END)
         msg1 = f"{sender}: {msg}\n\n"
@@ -131,20 +78,10 @@ class ChatApplication:
         self.text_widget.insert(END, msg1)
         self.text_widget.configure(state=DISABLED)
         
-        if translateT:
-            en_msg = nl_to_en(msg)
-            en_response = bot.get_response(en_msg).text
-            nl_response = en_to_nl(en_response)
-            msg2 = bot.name + ": " + nl_response + "\n\n"
-            self.text_widget.configure(state=NORMAL)
-            self.text_widget.insert(END, msg2)
-            self.text_widget.configure(state=DISABLED)
-        
-        else:
-            msg2 = bot.name + ": " + bot.get_response(msg).text + "\n\n"
-            self.text_widget.configure(state=NORMAL)
-            self.text_widget.insert(END, msg2)
-            self.text_widget.configure(state=DISABLED)
+        msg2 = f"{bot_name}: {get_response(msg)}\n\n"
+        self.text_widget.configure(state=NORMAL)
+        self.text_widget.insert(END, msg2)
+        self.text_widget.configure(state=DISABLED)
         
         self.text_widget.see(END)
              
